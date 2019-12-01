07eb86566e5d93259b048ecdca8dcca6d3e17124
ugwumadu116 <ugwumadu116@gmail.com>
2019-12-01 11:39:49 +0100

    added hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from "styled-components";
+import HeroImage from "../assets/img/56256.png";
+
+
+const Hero = () => {
+    const FlexContainer = styled.div`
+      height: 93vh;
+      margin: 0 auto;
+      margin-top: 7vh;
+      max-width: 1140px;
+      display: flex;
+      flex-flow: row wrap;
+      justify-content: center;
+      align-items: center;
+      & .hero_left {
+        max-width: 570px;
+        margin-right: 20px;
+        h1 {
+          font-size: 46px;
+          text-align: left;
+          color: #394e5d;
+        }
+        p {
+          text-align: left;
+          color: #394e5d;
+          font-size: 22px;
+        }
+        @media (max-width: 769px) {
+          h1 {
+          font-size: 2rem;
+        }
+      }
+      & .hero_left {
+        max-width: 570px;
+        img {
+          width: 100%;
+          height: 100%;
+        }
+      }
+    `;
+    return (
+      <FlexContainer>
+        <div className="hero_left">
+          <h1>Start new… Today!</h1>
+          <p>
+            Vivamus vestibulum elit efficitur, elementum sapien a, aliquet ipsum
+          </p>
+        </div>
+        <div className="hero_right">
+          <div className="imageContainer">
+            <img src={HeroImage} alt="Hero image"/>
+          </div>
+        </div>
+      </FlexContainer>
+    );
+}
+ 
+export default Hero;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Layout from '../components/Layout'
+import Hero from '../components/Hero'
 
 export function IndexPageTemplate({}) {
   return (
     <>
       <h1>Index Pagee</h1>
+      <Hero />
     </>
   )
 }
